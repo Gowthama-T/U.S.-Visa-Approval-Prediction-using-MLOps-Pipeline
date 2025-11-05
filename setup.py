dec731a80551613a81a8d9c36ba13db0b3505b81
Gowthama T <gowthamat634@gmail.com>
2025-11-05 12:40:26 +0530

    readme file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="US_visa",
+    version="0.0.0",
+    author="gowtham",
+    author_email="gowthama.t.salesforce@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
